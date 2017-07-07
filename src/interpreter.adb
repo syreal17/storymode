9d3837ecb8b5277846a543883498a660bebe805e
ltjones18 <luke.jones.ctr@usafa.edu>
2017-07-07 14:03:55 -0600

    Added discovery of new rooms and opening doors in all directions

--- src/interpreter.adb
+++ src/interpreter.adb
@@ -15,6 +15,28 @@ package body interpreter is
       end if;
    end;
    
+   procedure Discover(L : in out level.Level; X : Positive; Y : Positive; Old_D : Dungeon_Room) is
+      New_D : Dungeon_Room;
+   begin
+      if Is_Room(L, X, Y) then
+         New_D := Get_Room(L, X, Y);
+         if Old_D.ID /= New_D.ID then
+            New_D.Discovered := True;
+            Update_Room(L, New_D);
+         end if;
+      end if;
+   end Discover;
+   
+   procedure Try_Discovery_Cardinal(L : in out level.Level; X : Positive; Y : Positive) is
+      Old_D : Dungeon_Room;
+   begin
+      Old_D := Get_Room(L, X, Y);
+      Discover(L, X+1, Y, Old_D);
+      Discover(L, X-1, Y, Old_D);
+      Discover(L, X, Y+1, Old_D);
+      Discover(L, X, Y-1, Old_D);
+   end Try_Discovery_Cardinal;
+   
    procedure Open_Door(L : in out level.Level; X : Positive; Y : Positive) is
       D : Dungeon_Room;
       Room_X : Positive;
@@ -24,9 +46,10 @@ package body interpreter is
       D := Get_Room(L, X, Y);
       Room_X := X - D.X_Position + 1;
       Room_Y := Y - D.Y_Position + 1;
-      D.Board(Room_X, Room_Y) := O;
+      D.Board(Room_X, Room_Y) := abbr_tiles(Open_Door);
       --Print_Board(D);
       Update_Room(L, D);
+      Try_Discovery_Cardinal(L, X, Y);
    end Open_Door;
    
    --TODO: move not based on display tile, but underlying abstract tile
@@ -36,7 +59,10 @@ package body interpreter is
       if L.Current_Player.Y_Position > 1 then
          if Up_T = abbr_tiles(Floor) or 
           Up_T = abbr_tiles(Open_Door) then
-            L.Current_Player.X_Position := L.Current_Player.Y_Position - 1;
+            L.Current_Player.Y_Position := L.Current_Player.Y_Position - 1;
+         elsif Up_T = abbr_tiles(Closed_Door) or
+          Up_T = abbr_tiles(Secret_Door) then
+            Open_Door(L, L.Current_Player.X_Position, L.Current_Player.Y_Position - 1);   
          end if;
       end if;
    end Move_Up;
@@ -47,7 +73,10 @@ package body interpreter is
       if L.Current_Player.Y_Position < L.Current_Dungeon.Y_Length then
          if Down_T = abbr_tiles(Floor) or 
           Down_T = abbr_tiles(Open_Door) then
-            L.Current_Player.X_Position := L.Current_Player.Y_Position + 1;
+            L.Current_Player.Y_Position := L.Current_Player.Y_Position + 1;
+         elsif Down_T = abbr_tiles(Closed_Door) or
+          Down_T = abbr_tiles(Secret_Door) then
+            Open_Door(L, L.Current_Player.X_Position, L.Current_Player.Y_Position + 1);   
          end if;
       end if;
    end Move_Down;
@@ -59,6 +88,9 @@ package body interpreter is
          if Left_T = abbr_tiles(Floor) or
           Left_T = abbr_tiles(Open_Door) then
             L.Current_Player.X_Position := L.Current_Player.X_Position - 1;
+         elsif Left_T = abbr_tiles(Closed_Door) or
+          Left_T = abbr_tiles(Secret_Door) then
+            Open_Door(L, L.Current_Player.X_Position - 1, L.Current_Player.Y_Position);
          end if;
          --Put_Line("move left");
       end if;
@@ -71,7 +103,8 @@ package body interpreter is
          if Right_T = abbr_tiles(Floor) or
           Right_T = abbr_tiles(Open_Door) then
             L.Current_Player.X_Position := L.Current_Player.X_Position + 1;
-         elsif Right_T = abbr_tiles(Closed_Door) then
+         elsif Right_T = abbr_tiles(Closed_Door) or
+          Right_T = abbr_tiles(Secret_Door) then
             Open_Door(L, L.Current_Player.X_Position + 1, L.Current_Player.Y_Position);
          end if;
       end if;
