32d647c73f1664244a802dc1ada5a7939b9b3de8
ltjones18 <luke.jones.ctr@usafa.edu>
2017-07-07 18:00:10 -0600

    Added XP and HP to player

--- src/player.ads
+++ src/player.ads
@@ -3,6 +3,8 @@ package player is
   type Player is record
       X_Position : Positive;
       Y_Position : Positive;
+      XP         : Positive;
+      HP         : Positive;
    end record; 
 
 end player;
