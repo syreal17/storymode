5af04c9d58b685b2dac6e7e96d8b16f0fea62e3e
ltjones18 <luke.jones.ctr@usafa.edu>
2017-07-12 14:07:56 -0600

    Commented debug spec and body

--- src/debug.adb
+++ src/debug.adb
@@ -1,5 +1,7 @@
 package body debug is
 
+   --Print_Char_Codes: S: the string to print char codes. Prints numeric representation for each cahracter
+   --                  for an entire string.
    procedure Print_Char_Codes ( S : String ) is
    begin
       for C of S loop
@@ -9,6 +11,7 @@ package body debug is
       Put_Line("");
    end;
    
+   --Print_Board: D: the room from which to print a board. Used to verify that board is what we expect 
    procedure Print_Board ( D : Dungeon_Room ) is
    begin
       for Y in 1 .. D.Y_Length loop
--- src/debug.ads
+++ src/debug.ads
@@ -3,7 +3,11 @@ with dngn; use dngn;
 
 package debug is
 
-  procedure Print_Char_Codes ( S : String );
-  procedure Print_Board ( D : Dungeon_Room );
+   --Print_Char_Codes: S: the string to print char codes. Prints numeric representation for each cahracter
+   --                  for an entire string.
+   procedure Print_Char_Codes ( S : String );
+  
+   --Print_Board: D: the room from which to print a board. Used to verify that board is what we expect
+   procedure Print_Board ( D : Dungeon_Room );
   
 end debug;
